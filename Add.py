68364441b4c73a75e89e221782e47b1d408ad2d9
ShreyasK <shreyaskarevalli1@gmail.com>
2024-07-22 01:09:41 +0530

    Add three val

--- Add.py
+++ Add.py
@@ -1,10 +1,11 @@
 # Simple addition function
-def add_numbers(a, b):
-    return a + b
+def add_numbers(a, b,c):
+    return a + b+c
 
 # Example usage
 num1 = 10
 num2 = 5
+num3=10
 
-result = add_numbers(num1, num2)
-print(f"The sum of {num1} and {num2} is {result}")
+result = add_numbers(num1, num2,num3)
+print(f"The sum of {num1} and {num2} and {num3} is {result}")
