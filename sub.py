47e2871eb85e4bba0ac1886981374f398a96da23
ShreyasK <shreyaskarevalli1@gmail.com>
2024-07-21 23:32:59 +0530

    Three number subtraction

--- sub.py
+++ sub.py
@@ -1,9 +1,10 @@
-def subtract(a, b):
-    return a - b
+def subtract(a, b,c):
+    return a - b-c
 
 # Example usage:
 num1 = float(input("Enter the first number: "))
 num2 = float(input("Enter the second number: "))
+num3=float(input("Enter the third number: "))
 
-result = subtract(num1, num2)
-print(f"The result of subtracting {num2} from {num1} is {result}")
+result = subtract(num1, num2,num3)
+print(f"The result of subtracting {num2} from {num1} and {num3} is {result}")
